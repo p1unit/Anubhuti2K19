304d1aabc7355904648224c45598bcd4f16c28cc
Puneet Kumar <puneet16638@gmail.com>
2019-01-15 23:15:50 +0530

    event discription page added

---
+++ app/src/main/java/com/anubhuti/knit/Activities/EventDiscription.java
@@ -0,0 +1,65 @@
+package com.anubhuti.knit.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.anubhuti.knit.R;
+
+public class EventDiscription extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+        private static final int PERCENTAGE_TO_SHOW_IMAGE = 20;
+        private View mFab;
+        private int mMaxScrollSize;
+        private boolean mIsImageHidden;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_event_discription);
+
+            mFab = findViewById(R.id.flexible_example_fab);
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.flexible_example_toolbar);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+
+            AppBarLayout appbar = (AppBarLayout) findViewById(R.id.flexible_example_appbar);
+            appbar.addOnOffsetChangedListener(this);
+        }
+
+        @Override
+        public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+            if (mMaxScrollSize == 0)
+                mMaxScrollSize = appBarLayout.getTotalScrollRange();
+
+            int currentScrollPercentage = (Math.abs(i)) * 100
+                    / mMaxScrollSize;
+
+            if (currentScrollPercentage >= PERCENTAGE_TO_SHOW_IMAGE) {
+                if (!mIsImageHidden) {
+                    mIsImageHidden = true;
+
+                    ViewCompat.animate(mFab).scaleY(0).scaleX(0).start();
+                }
+            }
+
+            if (currentScrollPercentage < PERCENTAGE_TO_SHOW_IMAGE) {
+                if (mIsImageHidden) {
+                    mIsImageHidden = false;
+                    ViewCompat.animate(mFab).scaleY(1).scaleX(1).start();
+                }
+            }
+        }
+
+        public static void start(Context c) {
+            c.startActivity(new Intent(c, EventDiscription.class));
+        }
+}
