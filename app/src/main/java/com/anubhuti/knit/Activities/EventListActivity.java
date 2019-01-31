104e4c53314aac36f70e33268db0b3994ed3c9e5
Puneet Kumar <puneet16638@gmail.com>
2019-01-31 14:33:22 +0530

    toolbar attached

--- app/src/main/java/com/anubhuti/knit/Activities/EventListActivity.java
+++ app/src/main/java/com/anubhuti/knit/Activities/EventListActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.anubhuti.knit.Adapter.EventListAdapter;
 import com.anubhuti.knit.Interfaces.Detail_RegisterInterface;
@@ -20,6 +23,8 @@ import java.util.List;
 public class EventListActivity extends AppCompatActivity implements Detail_RegisterInterface {
 
     RecyclerView recyclerView;
+    TextView toolbarText;
+    ImageView backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +32,26 @@ public class EventListActivity extends AppCompatActivity implements Detail_Regis
         setContentView(R.layout.activity_event_list);
 
         recyclerView=this.findViewById(R.id.event_list_recycler);
+        toolbarText=this.findViewById(R.id.toolbar_text);
+        backbtn=this.findViewById(R.id.backbtn);
 
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
         Intent intent=getIntent();
-
         EventTypeResponse obj =(EventTypeResponse)intent.getSerializableExtra("listEvent");
 
+        toolbarText.setText(intent.getStringExtra("name"));
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
         List<EventTypeModel> lis=obj.getAllData();
-
         EventListAdapter eventListAdapter=new EventListAdapter(this,lis);
-
         recyclerView.setAdapter(eventListAdapter);
     }
 
