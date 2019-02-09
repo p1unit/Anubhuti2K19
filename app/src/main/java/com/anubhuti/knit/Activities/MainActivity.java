24fffbaef02a26f236e0b3f0694399850a2f8c77
athrva12 <upadhyayathrva@gmail.com>
2019-02-09 12:38:12 +0530

    splash

--- app/src/main/java/com/anubhuti/knit/Activities/MainActivity.java
+++ app/src/main/java/com/anubhuti/knit/Activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.anubhuti.knit.Activities;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.util.DiffUtil;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.anubhuti.knit.Migration.FireBaseData;
 import com.anubhuti.knit.Migration.UserMigration;
@@ -12,6 +15,7 @@ import com.anubhuti.knit.Model.PastFutureData;
 import com.anubhuti.knit.R;
 import com.anubhuti.knit.TinderSlider.CardStackAdapter;
 import com.anubhuti.knit.TinderSlider.SpotDiffCallback;
+import com.bumptech.glide.Glide;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackListener;
 import com.yuyakaido.android.cardstackview.CardStackView;
@@ -21,18 +25,31 @@ import com.yuyakaido.android.cardstackview.StackFrom;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements CardStackListener {
+public class MainActivity extends AppCompatActivity  {
 
     UserMigration userMigration;
-
-    private CardStackLayoutManager manager;
-    private CardStackAdapter adapter;
-    private CardStackView cardStackView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.splash);
+
+        Glide.with(this).asGif().load(R.raw.gif).into(imageView);
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+
+                        startApp();
+                    }
+                }, 2000);
+
+
+    }
+
+    private void startApp() {
 
         userMigration=new UserMigration();
         if(userMigration.isUserLogin()){
@@ -46,83 +63,12 @@ public class MainActivity extends AppCompatActivity implements CardStackListener
             startActivity(intent);
             finish();
         }
-
-//        initialize();
     }
 
-        @Override
+    @Override
     public void onDestroy() {
         super.onDestroy();
         Runtime.getRuntime().freeMemory();
-    }
-
-    @Override
-    public void onCardDragging(Direction direction, float ratio) {
-
-    }
-
-    @Override
-    public void onCardSwiped(Direction direction) {
-
-        if (manager.getTopPosition() == adapter.getItemCount() - 5) {
-            paginate();
-        }
-    }
-
-    @Override
-    public void onCardRewound() {
-
-    }
-
-    @Override
-    public void onCardCanceled() {
-
-    }
-
-    @Override
-    public void onCardAppeared(View view, int position) {
-
-    }
-
-    @Override
-    public void onCardDisappeared(View view, int position) {
-
-    }
-
-    private void initialize() {
-        manager = new CardStackLayoutManager(this, this);
-        manager.setStackFrom(StackFrom.Top);
-        manager.setVisibleCount(4);
-        manager.setTranslationInterval(8.0f);
-        manager.setScaleInterval(0.95f);
-        manager.setSwipeThreshold(0.1f);
-        manager.setMaxDegree(20.0f);
-        manager.setDirections(Direction.FREEDOM);
-        manager.setCanScrollHorizontal(true);
-        manager.setCanScrollVertical(true);
-        adapter = new CardStackAdapter(this, createSpots());
-        cardStackView = findViewById(R.id.card_stack_view);
-        cardStackView.setLayoutManager(manager);
-        cardStackView.setAdapter(adapter);
-    }
-
-    private void paginate() {
-        List<PastFutureData> oldList = adapter.getSpots();
-        List<PastFutureData> newList = new ArrayList<PastFutureData>() {{
-            addAll(adapter.getSpots());
-            addAll(createSpots());
-        }};
-        SpotDiffCallback callback = new SpotDiffCallback(oldList, newList);
-        DiffUtil.DiffResult result = DiffUtil.calculateDiff(callback);
-        adapter.setSpots(newList);
-        result.dispatchUpdatesTo(adapter);
-    }
-
-    private List< PastFutureData> createSpots() {
-
-        FireBaseData data=new FireBaseData();
-
-        return data.getGloriousPast();
     }
 
 
